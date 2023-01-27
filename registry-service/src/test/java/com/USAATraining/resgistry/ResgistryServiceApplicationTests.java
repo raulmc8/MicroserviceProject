8afe109f5318a3895beda1cd6f5e0939d1392f03
Raul <raul.marquez@therocketcode.com>
2023-01-27 11:50:35 -0600

    se agregan archivos de config

---
+++ registry-service/src/test/java/com/USAATraining/resgistry/ResgistryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.USAATraining.resgistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResgistryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
